c7704e293d0080a325d5599a8d5877bb177efc86
Natan Felipe de Freitas Souza <natanlipf@gmail.com>
2023-03-06 23:07:26 -0300

    Adicionados models de Cliente e Gerente

---
+++ ecommerce/ecommerce/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email é obrigatório')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class Gerente(AbstractBaseUser):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nome']
+
+    objects = CustomUserManager()
+
+class Cliente(AbstractBaseUser):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    endereco = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nome', 'endereco']
+
+    objects = CustomUserManager()
