7d880ccd322c8a33a64c2fa1f4825efe17df1e8e
Natan Felipe de Freitas Souza <natanlipf@gmail.com>
2023-03-09 00:30:26 -0300

    requirements modificado

--- ecommerce/build_staticfiles.sh
+++ ecommerce/build_staticfiles.sh
@@ -1,10 +1,7 @@
 # Install project requirements
 python3 -m pip install --upgrade pip
+pip install psycopg2-binary
 python3 -m pip install -r requirements.txt
 
 # Build staticfiles
 python3 manage.py collectstatic
-
-# Update database
-python3 manage.py makemigrations
-python3 manage.py migrate --run-syncdb
