ee313ed6c63b70109b13646557222150875272be
Natan Felipe de Freitas Souza <natanlipf@gmail.com>
2023-03-08 23:03:15 -0300

    Projeto configurado para deploy

---
+++ ecommerce/build_staticfiles.sh
@@ -0,0 +1,12 @@
+python3 --version
+pip --version 
+
+cat /etc/os-release
+
+# Install project requirements
+python3 -m pip install --upgrade pip
+pip install psycopg2-binary
+python3 -m pip install -r requirements.txt
+
+# Build staticfiles
+python3 manage.py collectstatic
